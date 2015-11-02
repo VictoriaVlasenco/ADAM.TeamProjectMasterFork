7581713b7f58075c186fa8ff34dd878730376416
roman-kozhedub <roman.kozhedub@gmail.com>
2015-11-02 19:23:40 +0300

    update

--- SharedFolderIndexer/SharedFolderIndexer.cs
+++ SharedFolderIndexer/SharedFolderIndexer.cs
@@ -1,11 +1,8 @@
 ﻿#region
 
-using System.IO;
-using System.Linq;
 using Adam.Core;
 using Adam.Core.Classifications;
 using Adam.Core.Indexer;
-using Adam.Core.Records;
 
 #endregion
 
@@ -13,32 +10,14 @@ namespace SharedFolderIndexer
 {
     public class SharedFolderIndexer : IndexMaintenanceJob
     {
-        private readonly string[] supportedExtensions = {".mp3", ".ogg", ".acc", ".wav"};
-
         public SharedFolderIndexer(Application app) : base(app)
         {
-        }
-
-        protected override void OnPreCatalog(PreCatalogEventArgs e)
-        {
-            base.OnPreCatalog(e);
-            if (supportedExtensions.Contains(Path.GetExtension(e.Path)))
-            {
-                e.Action = CatalogAction.AddRecord;
-            }
         }
 
         protected override void OnCatalog(CatalogEventArgs e)
         {
             base.OnCatalog(e);
-            if (e.Action == CatalogAction.AddRecord)
-            {
-                var record = new Record(App);
-                record.AddNew();
-                record.Classifications.Add(new ClassificationPath("/SoundCloud"), true);
-                record.Files.AddFile(e.Path);
-                record.Save();
-            }
+            e.Record.Classifications.Add(new ClassificationPath("/SoundCloud"), true);
         }
     }
 }
