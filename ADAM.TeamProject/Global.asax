f81e37e16770822a555bb90e02c6dfaabd9b05ee
roman-kozhedub <roman.kozhedub@gmail.com>
2015-11-02 10:48:14 +0300

    Created new project

---
+++ ADAM.TeamProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ADAM.TeamProject.MvcApplication" Language="C#" %>
---
+++ ADAM.TeamProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ADAM.TeamProject.Startup))]
+namespace ADAM.TeamProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
