f471891228c0cacbdf7fb7a3080fe8463c4d94ac
AlekseyKovalevsky <aleksey.kovalevsky1@gmail.com>
2015-11-03 16:33:24 +0300

    work with xml added

--- SharedFolderIndexer/SharedFolderIndexer.cs
+++ SharedFolderIndexer/SharedFolderIndexer.cs
@@ -1,8 +1,15 @@
 ﻿#region
 
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.XPath;
 using Adam.Core;
 using Adam.Core.Classifications;
+using Adam.Core.Fields;
 using Adam.Core.Indexer;
+using Adam.Core.Maintenance;
 
 #endregion
 
@@ -10,20 +17,46 @@ namespace SharedFolderIndexer
 {
     public class SharedFolderIndexer : IndexMaintenanceJob
     {
-        public SharedFolderIndexer(Application app) : base(app)
+        private readonly string[] extensionsSupported = {".mp3", ".wma", ".aac", ".flac"};
+
+        public SharedFolderIndexer(Application app)
+            : base(app)
         {
         }
 
         protected override void OnPreCatalog(PreCatalogEventArgs e)
         {
             base.OnPreCatalog(e);
-            e.Action = CatalogAction.AddRecord;
+            if (!extensionsSupported.Contains(Path.GetExtension(e.Path), StringComparer.InvariantCultureIgnoreCase))
+            {
+                e.Action = CatalogAction.Fail;
+                e.Message = string.Format("Cannot add file {0} - not supported extension", e.Path);
+            }
         }
 
         protected override void OnCatalog(CatalogEventArgs e)
         {
             base.OnCatalog(e);
-            e.Record.Classifications.Add(new ClassificationPath("/SoundCloud"), true);
-        }
+
+            XPathDocument metadataDocument = new XPathDocument(Path.GetDirectoryName(e.Path) + "/metadata.xml");
+            XPathNavigator metadataNavigator = metadataDocument.CreateNavigator();
+            XPathNodeIterator trackIterator = metadataNavigator.Select("library/track");
+            XmlNode tmpNode;
+            foreach (XmlNode track in trackIterator)
+            {
+                if (Path.GetFileName(e.Path).Equals(track.SelectSingleNode("fileName")))
+                {
+                    tmpNode = track.SelectSingleNode("title");
+                    if (tmpNode != null)
+                        e.Record.Fields.GetField<TextField>("Title").SetValue(tmpNode.Value);
+                    tmpNode = track.SelectSingleNode("artist");
+                    if (tmpNode != null)
+                        e.Record.Fields.GetField<TextField>("Artist").SetValue(tmpNode.Value);
+                    tmpNode = track.SelectSingleNode("genre");
+                    if (tmpNode != null)
+                        e.Record.Classifications.Add(new ClassificationPath("SoundCloud"+tmpNode.Value), true);
+                    break;
+                }
+            }
     }
 }
