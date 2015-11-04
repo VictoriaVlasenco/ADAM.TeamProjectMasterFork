ade7666db2366a53c225e569999f932c986ebb30
roman-kozhedub <roman.kozhedub@gmail.com>
2015-11-04 20:11:04 +0300

    Added  empty mvc 5 project

---
+++ SoundPlayer.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using System.Web.Mvc;
+
+#endregion
+
+namespace SoundPlayer.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SoundPlayer.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoundPlayer.Web.MvcApplication" Language="C#" %>
