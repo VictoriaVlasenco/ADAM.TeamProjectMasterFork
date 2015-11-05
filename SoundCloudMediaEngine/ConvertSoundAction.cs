9481e68d91892440a8d54490223818aaf369c7bb
AlekseyKovalevsky <aleksey.kovalevsky1@gmail.com>
2015-11-05 14:40:59 +0300

    MediaEngine model added

--- SoundCloudMediaEngine/ConvertSoundAction.cs
+++ SoundCloudMediaEngine/ConvertSoundAction.cs
@@ -1,6 +1,9 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Xml;
 using Adam.Core.MediaEngines;
 using Adam.Core.Records;
@@ -11,10 +14,27 @@ namespace SoundCloudMediaEngine
 {
     internal class ConvertSoundAction : MediaAction, ICatalogAction
     {
+        private readonly string[] formatsSupported = { ".mp3", ".wma", ".aac", ".flac" };
         private const string ActionId = "ConvertSoundAction";
+        private readonly string[] _formatsRequired;
+        private readonly string _originalFilePath;
+        public List<string> convertedFilesPaths  { get; set; }
 
-        public ConvertSoundAction(bool isCritical) : base(isCritical)
+        public string[] FormatsRequired
         {
+            get { return _formatsRequired; }
+        }
+        public ConvertSoundAction(string filepath,string[]formatsRequired,bool isCritical) : base(isCritical)
+        {
+            _formatsRequired = formatsRequired;
+            _originalFilePath = filepath;
+            convertedFilesPaths=new List<string>();
+        }
+
+        public ConvertSoundAction(CatalogActionData data):base(data.IsCritical)
+        {
+            _originalFilePath = data.Path;
+            _formatsRequired = formatsSupported.Except(new List<string>() {Path.GetExtension(data.Path)}).ToArray();
         }
 
         public override string Id
@@ -24,7 +44,10 @@ namespace SoundCloudMediaEngine
 
         public void UpdateFileVersion(FileVersion version, XmlWriter writer)
         {
-            throw new NotImplementedException();
+            foreach (string path in convertedFilesPaths)
+            {
+                version.AdditionalFiles.Add(path);
+            }
         }
     }
 }
---
+++ SoundCloudMediaEngine/SoundCloudMediaEngine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adam.Core;
+using Adam.Core.MediaEngines;
+
+namespace SoundCloudMediaEngine
+{
+    class SoundCloudMediaEngine:MediaEngine
+    {
+        private const string MediaEngineId = "SoundCloudMediaEngine";
+        public SoundCloudMediaEngine(Application app) : base(app)
+        {
+        }
+
+        public override string Id
+        {
+            get { return MediaEngineId; }
+        }
+
+        public override bool Run(MediaAction action)
+        {
+            switch (action.Id)
+            {
+                case "ConvertSoundAction":
+                    ConvertSound((ConvertSoundAction)action);
+                    return true;
+                default:
+                    return false;
+
+            }
+        }
+
+        private void ConvertSound(ConvertSoundAction action)
+        {
+            string tmpFilePath;
+            foreach (string format in action.FormatsRequired)
+            {
+                tmpFilePath = App.GetTemporaryFile(format);
+                /*convertion process here
+                 * result of convertion must be saved at 'tmpFilePath'
+                 */
+                action.convertedFilesPaths.Add(tmpFilePath);
+
+            }
+        }
+    }
+}
