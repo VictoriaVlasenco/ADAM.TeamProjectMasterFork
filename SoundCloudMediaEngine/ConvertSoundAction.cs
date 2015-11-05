9bdb46522bad0faf0dd2f63181bab37cd2e93652
roman-kozhedub <roman.kozhedub@gmail.com>
2015-11-05 11:06:16 +0300

    add CatalogAction

---
+++ SoundCloudMediaEngine/ConvertSoundAction.cs
@@ -0,0 +1,30 @@
+﻿#region
+
+using System;
+using System.Xml;
+using Adam.Core.MediaEngines;
+using Adam.Core.Records;
+
+#endregion
+
+namespace SoundCloudMediaEngine
+{
+    internal class ConvertSoundAction : MediaAction, ICatalogAction
+    {
+        private const string ActionId = "ConvertSoundAction";
+
+        public ConvertSoundAction(bool isCritical) : base(isCritical)
+        {
+        }
+
+        public override string Id
+        {
+            get { return ActionId; }
+        }
+
+        public void UpdateFileVersion(FileVersion version, XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
