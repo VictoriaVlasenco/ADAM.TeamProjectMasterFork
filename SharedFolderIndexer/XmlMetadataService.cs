7592bd231a9d2dc053800f07b66129a6ef7c9b1a
AlekseyKovalevsky <aleksey.kovalevsky1@gmail.com>
2015-11-06 19:44:03 +0300

    XmlMetadataService added

---
+++ SharedFolderIndexer/XmlMetadataService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Adam.Core.Fields;
+
+namespace SharedFolderIndexer
+{
+    class XmlMetadataService : IMetadataService
+    {
+        public string FilePath { get; set; }
+        public XmlMetadataService(string filePath)
+        {
+            FilePath = filePath;
+        }
+        public List<RecordMetadata> GetMetadataList()
+        {
+            if (FilePath == null)
+                return null;
+            if (!File.Exists(FilePath))
+                return null;
+            List<RecordMetadata> metadataList = new List<RecordMetadata>();
+            var metadataDocument = new XmlDocument();
+            metadataDocument.Load(FilePath);
+            XmlNode tracks = metadataDocument.DocumentElement;
+            XmlNode fileNameNode, titleNode, artistNode, genreNode;
+            foreach (XmlNode track in tracks.ChildNodes)
+            {
+                fileNameNode = track.SelectSingleNode("filename");
+                titleNode = track.SelectSingleNode("title");
+                artistNode = track.SelectSingleNode("artist");
+                genreNode = track.SelectSingleNode("genre");
+                metadataList.Add(new RecordMetadata()
+                {
+                    FileName = fileNameNode != null ? fileNameNode.InnerText : null,
+                    Title = titleNode != null ? titleNode.InnerText : null,
+                    Artist = artistNode != null ? artistNode.InnerText : null,
+                    Genre = genreNode != null ? genreNode.InnerText : null
+                });
+            }
+        }
+    }
+}
