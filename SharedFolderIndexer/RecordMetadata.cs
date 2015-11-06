1764b51ccb5cc16e4b89a4e263193473e97d5e7c
AlekseyKovalevsky <aleksey.kovalevsky1@gmail.com>
2015-11-06 18:58:14 +0300

    Fucking git deleted my work! I 'm Trying again

---
+++ SharedFolderIndexer/RecordMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedFolderIndexer
+{
+    class RecordMetadata
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+    }
+}
